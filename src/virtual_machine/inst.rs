53dabb7a2d2be349109a2406c0c356d2959219a7
creepersaur <69436784+creepersaur@users.noreply.github.com>
2026-05-26 02:29:07 +0530

    removed the checkmarks in `inst.rs`

--- src/virtual_machine/inst.rs
+++ src/virtual_machine/inst.rs
@@ -5,108 +5,108 @@ use crate::virtual_machine::value::Value;
 #[allow(unused, non_camel_case_types)]
 #[derive(Encode, Decode, Debug, Clone, PartialEq)]
 pub enum Inst {
-    COMMENT(String), // ✅
-    NOP,             // ✅
-    EXIT,            // ✅
-    PRINT,           // ✅
-    TO_STRING,       // ✅
-    DEFAULT,         // ✅
-    DEFAULT_NIL,     // ✅
-    PUSH(Value),     // ✅
-    DUP,             // ✅
-    SWAP,            // ✅
-    ROT3,            // ✅
-    POP,             // ✅
-    TRY_POP,         // ✅
+    COMMENT(String),
+    NOP,
+    EXIT,
+    PRINT,
+    TO_STRING,
+    DEFAULT,
+    DEFAULT_NIL,
+    PUSH(Value),
+    DUP,
+    SWAP,
+    ROT3,
+    POP,
+    TRY_POP,
 
     // Collections
-    RANGE,                    // ✅
-    LIST(usize),              // ✅
-    TUPLE(usize),             // ✅
-    DICT(usize),              // ✅
-    ENUM(String, Vec<Value>), // ✅
-    STRUCT(Vec<String>),      // ✅
+    RANGE,
+    LIST(usize),
+    TUPLE(usize),
+    DICT(usize),
+    ENUM(String, Vec<Value>),
+    STRUCT(Vec<String>),
     MAKE_CLASS {
         name: String,
         field_names: Vec<String>,
         field_consts: Vec<bool>,
         method_names: Vec<String>,
-		has_constructor: bool,
-    }, // ✅
-    INIT_CLASS(usize),        // ✅
+        has_constructor: bool,
+    },
+    INIT_CLASS(usize),
 
-    PATCH_ME(String), // ✅
+    PATCH_ME(String),
 
-    ADD, // ✅
-    SUB, // ✅
-    MUL, // ✅
-    DIV, // ✅
-    POW, // ✅
-    MOD, // ✅
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    POW,
+    MOD,
 
-    NEG, // ✅
-    POS, // ✅
+    NEG,
+    POS,
 
-    GT,  // ✅
-    LT,  // ✅
-    GE,  // ✅
-    LE,  // ✅
-    EQ,  // ✅
-    NEQ, // ✅
-    AND, // ✅
-    OR,  // ✅
-    NOT, // ✅
+    GT,
+    LT,
+    GE,
+    LE,
+    EQ,
+    NEQ,
+    AND,
+    OR,
+    NOT,
 
-    LOAD_CONST(usize),       // ✅
-    LOAD_GLOBAL(u64),        // ✅
-    STORE_GLOBAL(u64),       // ✅
-    STORE_GLOBAL_CONST(u64), // ✅
-    SET_VAR(u64),            // ✅
+    LOAD_CONST(usize),
+    LOAD_GLOBAL(u64),
+    STORE_GLOBAL(u64),
+    STORE_GLOBAL_CONST(u64),
+    SET_VAR(u64),
 
-    PUSH_SCOPE, // ✅
-    POP_SCOPE,  // ✅
+    PUSH_SCOPE,
+    POP_SCOPE,
     LOAD_LOCAL {
         id: u64,
         depth: usize,
-    }, // ✅
+    },
     STORE_LOCAL {
         id: u64,
         depth: usize,
-    }, // ✅
+    },
     STORE_LOCAL_CONST {
         id: u64,
         depth: usize,
-    }, // ✅
+    },
 
     // UpValues
     MAKE_CLOSURE {
         entry: usize,
         captures: Vec<usize>,
-    }, // ✅
+    },
     LOAD_UPVALUE {
         id: u64,
         scope_idx: usize,
-    }, // ✅
+    },
 
     // Load from local or global
-    LOAD(u64), // ✅
+    LOAD(u64),
 
-    JUMP(usize),            // ✅
-    JUMP_IF_FALSE(usize),   // ✅
-    JUMP_IF_TRUE(usize),    // ✅
-    JUMP_IF_NOT_NIL(usize), // ✅
+    JUMP(usize),
+    JUMP_IF_FALSE(usize),
+    JUMP_IF_TRUE(usize),
+    JUMP_IF_NOT_NIL(usize),
 
     // Get/Set property (member access)
-    GET_PROP, // ✅
-    SET_PROP, // ✅
+    GET_PROP,
+    SET_PROP,
 
-    CALL(usize),      // ✅
-    CALL_VOID(usize), // ✅
-    RETURN,           // ✅
+    CALL(usize),
+    CALL_VOID(usize),
+    RETURN,
 
     // Get iterator (for loop)
-    GET_ITER,        // ✅
-    FOR_ITER(usize), // ✅
+    GET_ITER,
+    FOR_ITER(usize),
 
     // Match statement
     MATCH,
