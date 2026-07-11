211863da2c1a19ad751c66a26eaa89b39d22674e
creepersaur <69436784+creepersaur@users.noreply.github.com>
2026-07-12 03:32:54 +0530

    File -> prefix, write_bytes, append, append_bytes, delete

--- src/virtual_machine/libs/namespaces/classes/file/file.rs
+++ src/virtual_machine/libs/namespaces/classes/file/file.rs
@@ -20,15 +20,23 @@ thread_local! {
             map.insert(hash_u64!("name"), lib_function_id!("File", "name"));
             map.insert(hash_u64!("extension"), lib_function_id!("File", "extension"));
             map.insert(hash_u64!("stem"), lib_function_id!("File", "stem"));
+            map.insert(hash_u64!("prefix"), lib_function_id!("File", "prefix"));
             map.insert(hash_u64!("is_file"), lib_function_id!("File", "is_file"));
             map.insert(hash_u64!("is_dir"), lib_function_id!("File", "is_dir"));
             map.insert(hash_u64!("parent"), lib_function_id!("File", "parent"));
+            map.insert(hash_u64!("rename"), lib_function_id!("File", "rename"));
+            map.insert(hash_u64!("move"), lib_function_id!("File", "move"));
+            map.insert(hash_u64!("copy"), lib_function_id!("File", "copy"));
 
 			// IO
 			map.insert(hash_u64!("read"), lib_function_id!("File", "read"));
 			map.insert(hash_u64!("read_bytes"), lib_function_id!("File", "read_bytes"));
 			map.insert(hash_u64!("read_exact"), lib_function_id!("File", "read_exact"));
             map.insert(hash_u64!("write"), lib_function_id!("File", "write"));
+            map.insert(hash_u64!("write_bytes"), lib_function_id!("File", "write_bytes"));
+            map.insert(hash_u64!("append"), lib_function_id!("File", "append"));
+            map.insert(hash_u64!("append_bytes"), lib_function_id!("File", "append_bytes"));
+            map.insert(hash_u64!("delete"), lib_function_id!("File", "delete"));
 
             map
         })),
--- src/virtual_machine/libs/namespaces/classes/file/file_lib.rs
+++ src/virtual_machine/libs/namespaces/classes/file/file_lib.rs
@@ -1,4 +1,7 @@
-use std::io::Read;
+use std::{
+    io::{Read, Write},
+    path::PathBuf,
+};
 
 use crate::{
     get_args,
@@ -60,6 +63,14 @@ impl FileLib {
         Value::string(file_data.path.file_stem().unwrap().to_str().unwrap())
     }
 
+    fn prefix(_vm: &mut VM, args: Vec<Value>) -> Value {
+        let [file] = get_args!(args, 1);
+
+        let file_data = Self::as_file_data(file, "File.prefix() can only be used on Files");
+
+        Value::string(file_data.path.file_prefix().unwrap().to_str().unwrap())
+    }
+
     fn size(_vm: &mut VM, args: Vec<Value>) -> Value {
         let [file] = get_args!(args, 1);
 
@@ -101,6 +112,50 @@ impl FileLib {
         let file_data = Self::as_file_data(file, "File.is_dir() can only be used on Files");
 
         Value::Bool(file_data.path.is_dir())
+    }
+
+    fn rename(_vm: &mut VM, args: Vec<Value>) -> Value {
+        let [file, new_name] = get_args!(args, 2);
+
+        let file_data = Self::as_file_data(file, "File.rename() can only be used on Files");
+
+        if file_data.path.exists() {
+            std::fs::rename(file_data.path, new_name.as_str()).expect("Could not rename file");
+
+            Value::NIL
+        } else {
+            panic!("Tried renaming non-existing file (`{:?}`)", file_data.path)
+        }
+    }
+
+    fn r#move(_vm: &mut VM, args: Vec<Value>) -> Value {
+        let [file, destination] = get_args!(args, 2);
+
+        let file_data = Self::as_file_data(file, "File.move() can only be used on Files");
+
+        if file_data.path.exists() {
+            let path_buf =
+                PathBuf::from(destination.as_str()).join(file_data.path.file_name().unwrap());
+            std::fs::rename(file_data.path, path_buf).expect("Could not move file");
+
+            Value::NIL
+        } else {
+            panic!("Tried moving non-existing file (`{:?}`)", file_data.path)
+        }
+    }
+
+    fn copy(_vm: &mut VM, args: Vec<Value>) -> Value {
+        let [file, new_name] = get_args!(args, 2);
+
+        let file_data = Self::as_file_data(file, "File.copy() can only be used on Files");
+
+        if file_data.path.exists() {
+            std::fs::copy(file_data.path, new_name.as_str()).expect("Could not copy file");
+
+            Value::NIL
+        } else {
+            panic!("Tried copying non-existing file (`{:?}`)", file_data.path)
+        }
     }
 
     // fn parent(_vm: &mut VM, args: Vec<Value>) -> Value {
@@ -178,6 +233,102 @@ impl FileLib {
 
         Value::NIL
     }
+
+    fn write_bytes(_vm: &mut VM, args: Vec<Value>) -> Value {
+        let [file, contents] = get_args!(args, 2);
+
+        let file_data = Self::as_file_data(file, "File.write_bytes() can only be used on Files");
+
+        let mut file = std::fs::OpenOptions::new()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open(&file_data.path)
+            .expect("Could not open file for writing bytes");
+
+        if let Value::List(TList { values, .. }) = contents {
+            file.write(
+                &values
+                    .borrow()
+                    .iter()
+                    .map(|x| x.as_number("number convertion") as u8)
+                    .collect::<Vec<_>>(),
+            )
+            .expect("Could not append bytes to file");
+        } else {
+            panic!("Expected list of bytes (numbers), got {contents:?}")
+        }
+
+        Value::NIL
+    }
+
+    fn append(_vm: &mut VM, args: Vec<Value>) -> Value {
+        let [file, contents] = get_args!(args, 2);
+
+        let file_data = Self::as_file_data(file, "File.append() can only be used on Files");
+
+        if file_data.path.exists() {
+            std::fs::OpenOptions::new()
+                .create(true)
+                .append(true)
+                .open(file_data.path)
+                .expect("Could not open file for appending")
+                .write_all(contents.as_str().as_bytes())
+                .expect("Could not append to file");
+        } else {
+            panic!(
+                "Tried appending to non-existing file (`{:?}`)",
+                file_data.path
+            )
+        }
+
+        Value::NIL
+    }
+
+    fn append_bytes(_vm: &mut VM, args: Vec<Value>) -> Value {
+        let [file, contents] = get_args!(args, 2);
+
+        let file_data = Self::as_file_data(file, "File.append_bytes() can only be used on Files");
+
+        if file_data.path.exists() {
+            if let Value::List(TList { values, .. }) = contents {
+                std::fs::OpenOptions::new()
+                    .create(true)
+                    .append(true)
+                    .open(file_data.path)
+                    .expect("Could not open file for appending")
+                    .write_all(
+                        &values
+                            .borrow()
+                            .iter()
+                            .map(|x| x.as_number("number convertion") as u8)
+                            .collect::<Vec<_>>(),
+                    )
+                    .expect("Could not append bytes to file");
+            }
+        } else {
+            panic!(
+                "Tried appending bytes to non-existing file (`{:?}`)",
+                file_data.path
+            )
+        }
+
+        Value::NIL
+    }
+
+    fn delete(_vm: &mut VM, args: Vec<Value>) -> Value {
+        let [file] = get_args!(args, 1);
+
+        let file_data = Self::as_file_data(file, "File.delete() can only be used on Files");
+
+        if file_data.path.exists() {
+            std::fs::remove_file(file_data.path).expect("Could not delete file");
+        } else {
+            panic!("Tried deleting non-existing file (`{:?}`)", file_data.path)
+        }
+
+        Value::NIL
+    }
 }
 
 // Library
@@ -193,10 +344,14 @@ impl Library for FileLib {
             x if x == hash_u64!("name") => boxed!(Self::name),
             x if x == hash_u64!("extension") => boxed!(Self::extension),
             x if x == hash_u64!("stem") => boxed!(Self::stem),
+            x if x == hash_u64!("prefix") => boxed!(Self::prefix),
             x if x == hash_u64!("is_file") => boxed!(Self::is_file),
             x if x == hash_u64!("is_dir") => boxed!(Self::is_dir),
             x if x == hash_u64!("exists") => boxed!(Self::exists),
             x if x == hash_u64!("size") => boxed!(Self::size),
+            x if x == hash_u64!("rename") => boxed!(Self::rename),
+            x if x == hash_u64!("move") => boxed!(Self::r#move),
+            x if x == hash_u64!("copy") => boxed!(Self::copy),
             // x if x == hash_u64!("parent") => boxed!(Self::parent),
 
             // IO
@@ -204,6 +359,10 @@ impl Library for FileLib {
             x if x == hash_u64!("read_bytes") => boxed!(Self::read_bytes),
             x if x == hash_u64!("read_exact") => boxed!(Self::read_exact),
             x if x == hash_u64!("write") => boxed!(Self::write),
+            x if x == hash_u64!("write_bytes") => boxed!(Self::write_bytes),
+            x if x == hash_u64!("append") => boxed!(Self::append),
+            x if x == hash_u64!("append_bytes") => boxed!(Self::append_bytes),
+            x if x == hash_u64!("delete") => boxed!(Self::delete),
 
             _ => panic!("Unknown function `{name}` on lib {}", self.get_name()),
         }
